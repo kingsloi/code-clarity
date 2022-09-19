097c8022ea1e76d7e0d0e29b861430c20bb4db64
Kingsley Raspe <kingsley.raspe@googlemail.com>
2022-09-18 22:03:04 -0500

    default to public/pdf/* if docparser file has been deleted

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -8,35 +8,41 @@ use App\Models\Scan;
 
 use App\Http\Controllers\ScanController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/import', [ScanController::class, 'import']);
-
 Route::get('/', function () {
-
-    $statement = Statement::create();
-    $pages = Page::all();
-    $page = Statement::with(['pages', 'pages.charges', 'pages.payments'])->first();
-
-    dd($page->toArray());
+    return Statement::with(['pages', 'pages.charges', 'pages.payments'])->get();
 });
 
+Route::get('/import', [ScanController::class, 'import']);
 
 Route::get('/scans/{scanId}/pdf', function (Request $request, $scanId) {
     $scan = Scan::find($scanId);
 
+    // Get the scanned document's original filename
+    $filename = $scan->document->file_name;
+
+    // by default, grab the Docparse.com media link to view the PDF
     $url = $scan->document->media_link;
 
-    $data = file_get_contents($url);
+    // Docparse.com deletes files after x (30 days), so as to not lose
+    // the relationships between previously entered statements
+    // we re-upload the documents to docparser, and save
+    // the new files to the another table, so we can
+    // still access the docparser media_link as
+    // this preserves the OCR - meaning it's
+    // clickable/selectable text
+    $rescan = DB::table('refresh_scans')->whereJsonContains('document->file_name', $filename)->first();
+
+    // if a rescan has been found
+    if ($rescan) {
+        $json = json_decode($rescan->document);
+        $url = $json->media_link;
+    }
+
+    try {
+        $data = file_get_contents($url);
+    } catch (Exception $e) {
+        $data = file_get_contents(public_path('pdfs/' . $filename));
+    }
 
     header('Content-Type: application/pdf');
 
