8b99ca9b3a908e645a4bda3ed5cc6392c41c58e6
Kingsley Raspe <kingsley.raspe@googlemail.com>
2022-09-18 21:14:48 -0500

    revert code to load pdf from Docparser.com

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -34,14 +34,9 @@ Route::get('/', function () {
 Route::get('/scans/{scanId}/pdf', function (Request $request, $scanId) {
     $scan = Scan::find($scanId);
 
-    $filename = $scan->document->file_name;
-    $test = DB::table('refresh_scans')->whereJsonContains('document->file_name', $filename)->first();
-    $json = json_decode($test->document);
-    $url = $json->media_link;
-    $data = file_get_contents($url);
+    $url = $scan->document->media_link;
 
-    // $url = $scan->document->file_name;
-    // $data = file_get_contents(public_path('pdfs/' . $url));
+    $data = file_get_contents($url);
 
     header('Content-Type: application/pdf');
 
